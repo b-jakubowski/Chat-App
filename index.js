1a4a57e01b1520e449d11b9b74aa6a56a7aa8173
bj <bjakubov@gmail.com>
2018-01-11 09:20:35 +0100

    fixed

--- index.js
+++ index.js
@@ -6,31 +6,26 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const UsersService = require('./UsersService');
-
 const userService = new UsersService();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(socket) {
-	//miejsce dla funkcji wykonanej po podl. uzytkownika
-	//nasluchiwanie wejscia na czat
-	socket.on('join', function(name) {
-		// użytkownika, który pojawił się w aplikacji zapisujemy do serwisu trzymającego listę osób w czacie
+io.on('connection', socket => {
+	socket.on('join', name => {
 		userService.addUser({
 			id: socket.id,
 			name
 		});
-		// aplikacja emituje zdarzenie update, które aktualizuje informację na temat listy użytkowników każdemu nasłuchującemu na wydarzenie 'update'
+
 		io.emit('update', {
 			users: userService.getAllUsers()
 		});
 	});
 
-	//rozłączenie z serwerem, użytkownik wychodzi z czatu
 	socket.on('disconnect', () => {
 		userService.removeUser(socket.id);
 		socket.broadcast.emit('update', {
@@ -38,8 +33,7 @@ io.on('connection', function(socket) {
 		});
 	});
 
-	//przesyłanie wiadomości
-	socket.on('message', function(socket) {
+	socket.on('message', message => {
 		const {name} = userService.getUserById(socket.id);
 		socket.broadcast.emit('message', {
 			text: message.text,
@@ -48,7 +42,4 @@ io.on('connection', function(socket) {
 	});
 });
 
-server.listen(3000, function(){
-	console.log('listening on *:3000');
-});
-
+server.listen(3000, () => console.log('listening on *:3000'));
